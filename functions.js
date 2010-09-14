2b14967e0f878ea32ea0709070ec4822b8f89fe1
smithd <nobody@users.noreply.github.com>
2010-09-14 01:44:59 +0000

    Added the AJAX JS methods for retrieving the date from the session and redirecting to the login page if the session has expired.
    Does not actually retrieve the date more than once - need to resolve some date formatting issues with the tagfiler server because the format used is not supported by javascript's date.

--- functions.js
+++ functions.js
@@ -3,3 +3,51 @@ function setDatasetLink(div_id, datasetLink) {
         "'>" + datasetLink + "</a>";
   document.getElementById(div_id).innerHTML = html_link;
 }
+
+/**
+ * Starts the session check timer with a given delay time (millis)
+ */
+function startSessionTimer(t) {
+  setTimeout("runSessionRequest()", t);
+}
+
+/**
+ * Runs the Ajax request to retrieve the expiration
+ */
+function runSessionRequest() {
+  if(ajax_request) {
+    ajax_request.open("GET", expiration_check_url);
+    ajax_request.send(null);
+  }
+}
+
+/**
+ * Processes the response from the Ajax request
+ */
+function processSessionRequest() {
+  if(ajax_request.readyState == 4) {
+    if(ajax_request.status == 200) {
+      expire_date_str = unescape(ajax_request.responseText.replace("until=", ""));
+      // update time variable with new expiration time and restart
+      // startSessionTimer();
+    }
+    else if(ajax_request.status == 404) {
+      // redirect to the login page
+      window.location='/webauthn/login';
+    }
+  }
+}
+
+var expiration_check_url = "/webauthn/session";
+var ajax_request = null;
+if(window.ActiveXObject) {
+  ajax_request = new ActiveXObject("Microsoft.XMLHTTP");
+}
+else if(window.XMLHttpRequest) {
+  ajax_request = new XMLHttpRequest();
+}
+
+if(ajax_request) {
+  ajax_request.onreadystatechange = processSessionRequest;
+}
+
