417e9ff00fbaae29c2c8ad2621b4316745f7c376
svoinea <serban@isi.edu>
2011-04-05 01:29:24 +0000

    Defined GUI immutable tags.

--- dataserv_app.py
+++ dataserv_app.py
@@ -482,6 +482,7 @@ class Application:
         self.globals['clientDownloadChunks'] = self.config['client download chunks']
         self.globals['clientSocketBufferSize'] = self.config['client socket buffer size']
         self.globals['clientRetryCount'] = self.config['client retry count']
+        self.globals['browsersImmutableTags'] = [ 'check point offset', 'immutable exempt', 'key', 'sha256sum' ]
         
         # END: get runtime parameters from database
 
