6db395fb8113e0c13ada15dd610ef298d38854b3
svoinea <serban@isi.edu>
2011-04-06 01:04:56 +0000

    Fixed bug for removing a read/write user.

--- url_ast.py
+++ url_ast.py
@@ -1032,9 +1032,9 @@ class FileTags (Node):
                         self.delete_tag(subject, tagdef, val)
             
                     if tag in [ 'read users', 'write users' ]:
-                        for subfile in subfiles:
+                        for subfile in self.subfiles.values():
                             self.enforce_tag_authz('write', subfile, tagdef)
-                            self.txlog('DELETE', dataset=self.subject2identifiers(subfile)[0], tag=self.tag_id, value=((vals[0]!=None) and ','.join([str(val) for val in vals])) or None)
+                            self.txlog('DELETE', dataset=self.subject2identifiers(subfile)[0], tag=tag, value=((vals[0]!=None) and ','.join([str(val) for val in vals])) or None)
                             for val in vals:
                                 self.delete_tag(subfile, tagdef, val)
 
