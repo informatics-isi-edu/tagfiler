deee23d57de5c759dc878db268e03833a0db2fcf
karlcz <karlcz@users.noreply.github.com>
2011-08-05 20:54:04 +0000

    missing shell escapes for $N parameter references in SQL procedure embedded in shell here-document

--- dbsetup.sh
+++ dbsetup.sh
@@ -548,14 +548,14 @@ CREATE FUNCTION
   resources_authzinfo ( roles text[], ignore_read_authz boolean = False ) RETURNS TABLE ( subject int8, txid int8, readok boolean, writeok boolean ) AS \$\$
   SELECT r.subject AS subject, 
          t.value AS txid,
-         bool_or(ru.value = ANY ($1) OR o.value = ANY ($1)) AS readok, 
-         bool_or(wu.value = ANY ($1) OR o.value = ANY ($1)) AS writeok
+         bool_or(ru.value = ANY (\$1) OR o.value = ANY (\$1)) AS readok, 
+         bool_or(wu.value = ANY (\$1) OR o.value = ANY (\$1)) AS writeok
   FROM resources r
   LEFT OUTER JOIN "_subject last tagged txid" t ON (r.subject = t.subject)
-  LEFT OUTER JOIN "_owner" o ON (r.subject = o.subject AND o.value = ANY ($1))
-  LEFT OUTER JOIN "_read users" ru ON (r.subject = ru.subject AND ru.value = ANY ($1))
-  LEFT OUTER JOIN "_write users" wu ON (r.subject = wu.subject AND wu.value = ANY ($1))
-  WHERE $2 or o.value IS NOT NULL OR ru.value IS NOT NULL
+  LEFT OUTER JOIN "_owner" o ON (r.subject = o.subject AND o.value = ANY (\$1))
+  LEFT OUTER JOIN "_read users" ru ON (r.subject = ru.subject AND ru.value = ANY (\$1))
+  LEFT OUTER JOIN "_write users" wu ON (r.subject = wu.subject AND wu.value = ANY (\$1))
+  WHERE \$2 or o.value IS NOT NULL OR ru.value IS NOT NULL
   GROUP BY r.subject, txid
 \$\$ LANGUAGE SQL;
 EOF
