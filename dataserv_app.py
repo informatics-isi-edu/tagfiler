d20f9e41bceab3b526be818e1f44b492cca7b444
karlcz <karlcz@users.noreply.github.com>
2013-02-01 01:46:38 +0000

    don't mutate spreds since it can break later code using the same input path structure

--- dataserv_app.py
+++ dataserv_app.py
@@ -3595,7 +3595,7 @@ class Application (webauthn2_handler_factory.RestHandler):
                     lpreds.append( web.Storage(tag='id', op=None, vals=[]) )
 
             if enforce_read_authz and final and not json:
-                spreds.append( web.Storage(tag='readok', op='=', vals=[True]) )
+                spreds = spreds + [ web.Storage(tag='readok', op='=', vals=[True]) ]
                 
             spreds = self.mergepreds(spreds, tagdefs)
             lpreds = self.mergepreds(lpreds, tagdefs)
