2a069d50d9668b60e7d23344bd44c0f811f89362
karlcz <karlcz@users.noreply.github.com>
2013-05-17 22:26:29 +0000

    add missing import statement after moving /query/ code to /subject/ handler

--- src/tagfiler/subjects.py
+++ src/tagfiler/subjects.py
@@ -29,7 +29,7 @@ import time
 import datetime
 import pytz
 
-from dataserv_app import Application, NotFound, BadRequest, Conflict, RuntimeError, Forbidden, urlquote, urlunquote, parseBoolString, predlist_linearize, path_linearize, wraptag, jsonFileReader, jsonArrayFileReader, JSONArrayError, jsonWriter, getParamEnv, make_temporary_file
+from dataserv_app import Application, NotFound, BadRequest, Conflict, RuntimeError, Forbidden, urlquote, urlunquote, parseBoolString, predlist_linearize, path_linearize, wraptag, jsonFileReader, jsonArrayFileReader, JSONArrayError, jsonWriter, getParamEnv, make_temporary_file, yieldBytes
 
 myrand = random.Random()
 myrand.seed(os.getpid())
